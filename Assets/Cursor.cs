05a19e5416e554350be22178d6ea75d847b8ec0f
Komstuch <komstuchxbox@gmail.com>
2018-06-12 19:03:11 +0200

    Add simple Raycaster

---
+++ Assets/Cursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    CameraRaycaster cameraRaycaster;
+
+	void Start () {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+	}
+	
+	void Update () {
+        print(cameraRaycaster.layerHit);
+	}
+}
